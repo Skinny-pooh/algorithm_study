ce2f0721e92bdb9cceff4a27470c29b68b9baa63
Skinny-pooh <82829380+Skinny-pooh@users.noreply.github.com>
2021-09-24 00:43:29 +0900

    Create greedy_ATM.java

---
+++ greedy_ATM.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main{
+	 static int N;
+	public static void main(String[] arg) {
+		Scanner input = new Scanner(System.in);
+		N = input.nextInt();
+		int[] arr = new int[N];
+		for(int i=0;i<N;i++) {
+			arr[i] = input.nextInt();
+		}
+		ATM(arr);
+	}
+	
+	private static void ATM(int[] array) {
+		int[] arr = new int[N];
+		Arrays.fill(arr, 0);
+		int temp;
+		int result=0;
+		for(int i=0;i<N;i++)
+		{
+			for(int j=0;j<N;j++) {
+				if(array[i]<array[j]) {
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				}
+			}
+		}
+		for(int i =N-1;i<N;i--) {
+			 for(int j =0;j<i+1;j++) {
+				arr[i] += array[j];
+				 
+			 }
+		}
+		for(int i=0;i<N;i++) {
+			result += arr[i];
+		}
+		System.out.print(result);
+	}
+}
